6faab5dcb230b5d535e3713ae63fe3923b5253d0
odrevet <odrevet@gmail.com>
2022-10-26 22:47:48 +0200

    hints

--- image_single/build_assets.sh
+++ image_single/build_assets.sh
@@ -1,5 +1,5 @@
 image=$1
 convert "$image" -resize 160x144! +dither -colors 4 "./assets/image_gb.png"
 if png2asset "./assets/image_gb.png" -map -c "src/image.c" | grep -q "found more than 256 tiles"; then
-    echo "ERROR found more than 256 tiles: This picture cannot be converted because it does not contains enough repeating patterns"
+    echo "ERROR found more than 256 tiles: This picture cannot be converted because it does not contains enougth repeating patterns. Try again with -colors 2 or -border 2.5x2.5 options"
 fi
