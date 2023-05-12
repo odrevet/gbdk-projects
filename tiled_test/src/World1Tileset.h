eefa5a301de97ed652fcfd7137859140deede6d5
odrevet <odrevet@gmail.com>
2023-05-12 20:40:21 +0200

    tiled almost ok

--- tiled_test/src/World1Tileset.h
+++ tiled_test/src/World1Tileset.h
@@ -9,9 +9,9 @@
 #define World1Tileset_TILE_ORIGIN 0
 #define World1Tileset_TILE_W 8
 #define World1Tileset_TILE_H 8
-#define World1Tileset_WIDTH 224
+#define World1Tileset_WIDTH 48
 #define World1Tileset_HEIGHT 8
-#define World1Tileset_TILE_COUNT 28
+#define World1Tileset_TILE_COUNT 5
 #define World1Tileset_PALETTE_COUNT 1
 #define World1Tileset_COLORS_PER_PALETTE 4
 #define World1Tileset_TOTAL_COLORS 4
@@ -19,7 +19,7 @@
 BANKREF_EXTERN(World1Tileset)
 
 extern const palette_color_t World1Tileset_palettes[4];
-extern const uint8_t World1Tileset_tiles[448];
+extern const uint8_t World1Tileset_tiles[80];
 
 
 #endif
