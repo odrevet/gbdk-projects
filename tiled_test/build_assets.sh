4cfd22e15341047d9a92794cc351a93a7c696edd
odrevet <odrevet@gmail.com>
2023-05-14 11:19:15 +0200

    autobank

--- tiled_test/build_assets.sh
+++ tiled_test/build_assets.sh
@@ -1,2 +1,2 @@
 png2asset "./assets/World1Tileset.png" -c "src/World1Tileset.c" -spr8x8 -tiles_only
-node ../../tiled-gameboy-tool/build/index.js --output-dir src --tiled assets/World1Area1.tmx
+node ../../tiled-gameboy-tool/build/index.js --output-dir src --tiled assets/World1Area1.tmx --bank AUTOBANKED
