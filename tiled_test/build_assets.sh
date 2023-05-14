0dd4fd9dab7244b5d75e701b4e90a50702db3ac2
odrevet <odrevet@gmail.com>
2023-05-14 12:18:02 +0200

    update assets

--- tiled_test/build_assets.sh
+++ tiled_test/build_assets.sh
@@ -1,3 +1,3 @@
-png2asset "./assets/World1Tileset.png" -c "src/World1Tileset.c" -spr8x8 -tiles_only
+png2asset "./assets/World1Tileset.png" -c "src/World1Tileset.c" -spr8x8 -tiles_only -keep_duplicate_tiles
 node ../../tiled-gameboy-tool/build/index.js --output-dir src --tiled assets/World1Area1.tmx --bank AUTOBANKED
 sed -i '1d' src/world1area1.c
--- tiled_test/src/World1Tileset.h
+++ tiled_test/src/World1Tileset.h
@@ -9,17 +9,17 @@
 #define World1Tileset_TILE_ORIGIN 0
 #define World1Tileset_TILE_W 8
 #define World1Tileset_TILE_H 8
-#define World1Tileset_WIDTH 48
+#define World1Tileset_WIDTH 224
 #define World1Tileset_HEIGHT 8
-#define World1Tileset_TILE_COUNT 5
-#define World1Tileset_PALETTE_COUNT 1
+#define World1Tileset_TILE_COUNT 27
+#define World1Tileset_PALETTE_COUNT 2
 #define World1Tileset_COLORS_PER_PALETTE 4
-#define World1Tileset_TOTAL_COLORS 4
+#define World1Tileset_TOTAL_COLORS 8
 
 BANKREF_EXTERN(World1Tileset)
 
-extern const palette_color_t World1Tileset_palettes[4];
-extern const uint8_t World1Tileset_tiles[80];
+extern const palette_color_t World1Tileset_palettes[8];
+extern const uint8_t World1Tileset_tiles[432];
 
 
 #endif
