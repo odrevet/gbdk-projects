bdf0dc6df95e520d1384525a517de9bdb7cbfadd
odrevet <odrevet@gmail.com>
2023-05-14 15:00:22 +0200

    build map OK

--- tiled_test/src/World1Tileset.h
+++ tiled_test/src/World1Tileset.h
@@ -11,7 +11,7 @@
 #define World1Tileset_TILE_H 8
 #define World1Tileset_WIDTH 224
 #define World1Tileset_HEIGHT 8
-#define World1Tileset_TILE_COUNT 27
+#define World1Tileset_TILE_COUNT 28
 #define World1Tileset_PALETTE_COUNT 2
 #define World1Tileset_COLORS_PER_PALETTE 4
 #define World1Tileset_TOTAL_COLORS 8
@@ -19,7 +19,7 @@
 BANKREF_EXTERN(World1Tileset)
 
 extern const palette_color_t World1Tileset_palettes[8];
-extern const uint8_t World1Tileset_tiles[432];
+extern const uint8_t World1Tileset_tiles[448];
 
 
 #endif
