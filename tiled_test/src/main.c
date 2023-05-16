791ff0413104497a9a939d0ff5001ca33b3891a3
odrevet <odrevet@gmail.com>
2023-05-16 12:13:57 +0200

    display tile info under cursor OK

--- tiled_test/src/main.c
+++ tiled_test/src/main.c
@@ -28,7 +28,7 @@ void main(void) {
   int joypad_previous, joypad_current;
 
   // cursor
-  int cursor_x = 6 - OFFSET_X, cursor_y = 6 - OFFSET_Y;
+  int cursor_x = (SCREEN_WIDTH / 2), cursor_y = (SCREEN_HEIGHT / 2);
   set_sprite_data(0, 1, cursor_tiles);
   set_sprite_tile(0, 0);
   move_sprite(0, cursor_x * TILE_SIZE, cursor_y * TILE_SIZE);
@@ -41,13 +41,13 @@ void main(void) {
     joypad_previous = joypad_current;
     joypad_current = joypad();
 
-    if (joypad_current & J_RIGHT && !(joypad_previous & J_RIGHT))
+    if (joypad_current & J_RIGHT && !(joypad_previous & J_RIGHT) && cursor_x < world1area1_WIDTH)
       cursor_x++;
-    if (joypad_current & J_LEFT && !(joypad_previous & J_LEFT))
+    if (joypad_current & J_LEFT && !(joypad_previous & J_LEFT) && cursor_x > 1)
       cursor_x--;
-    if (joypad_current & J_UP && !(joypad_previous & J_UP))
+    if (joypad_current & J_UP && !(joypad_previous & J_UP) && cursor_y > 2)
       cursor_y--;
-    if (joypad_current & J_DOWN && !(joypad_previous & J_DOWN))
+    if (joypad_current & J_DOWN && !(joypad_previous & J_DOWN) && cursor_y <= world1area1_HEIGHT)
       cursor_y++;
 
     if (joypad_current != joypad_previous) {
@@ -58,8 +58,8 @@ void main(void) {
       char buffer[SCREEN_WIDTH * 2];
       int index = (cursor_y - 2) * world1area1_WIDTH + (cursor_x - 1);
       sprintf(buffer, "X:%d Y:%d INDEX:%d\nTILE:%d ATTR:%d",
-              (int16_t)cursor_x - 1, (int16_t)cursor_y - 2, (int16_t)index,
-              world1area1_map[world1area1_WIDTH * cursor_y + cursor_x],
+              (int16_t)cursor_x - OFFSET_X, (int16_t)cursor_y - OFFSET_Y, (int16_t)index,
+              world1area1_map[world1area1_WIDTH * (cursor_y - OFFSET_Y) + (cursor_x - OFFSET_X)],
               (int16_t)world1area1_map_attributes[index]);
       text_print_string_bkg(0, 0, buffer);
     }
