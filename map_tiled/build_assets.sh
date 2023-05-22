ed641792901524ed9dc8488a3d6219258bfcaaa7
odrevet <odrevet@gmail.com>
2023-05-22 10:34:37 +0200

    do not build area2

--- map_tiled/build_assets.sh
+++ map_tiled/build_assets.sh
@@ -3,5 +3,4 @@ png2asset "./assets/cursor.png" -c "src/graphics/cursor.c" -spr8x8 -tiles_only
 png2asset "./assets/World1Tileset.png" -c "src/graphics/World1Tileset.c" -spr8x8 -tiles_only -keep_duplicate_tiles -map
 node ../../tiled-gameboy-tool/build/index.js --output-dir src/maps --tiled assets/World1Area1.tmx --bank AUTOBANKED -export-solid-map
 sed -i '1d' src/maps/world1area1.c
-node ../../tiled-gameboy-tool/build/index.js --output-dir src/maps --tiled assets/World1Area2.tmx --bank AUTOBANKED -export-solid-map
-sed -i '1d' src/maps/world1area2.c
+
