8fb12ba3f6cb6c39ba1f95a6cad1f6d0b4805b27
Olivier Drevet <odrevet@gmail.com>
2023-05-25 22:24:09 +0200

    add project marioland

---
+++ marioland/src/main.c
@@ -0,0 +1,239 @@
+#include <gb/gb.h>
+#include <gb/metasprites.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "graphics/World1Tileset.h"
+#include "graphics/mario.h"
+#include "maps/world1area1.h"
+#include "maps/world1area2.h"
+
+#include "text.h"
+
+#define TILE_SIZE 8
+#define OFFSET_X 1
+#define OFFSET_Y 2
+#define SCREEN_HEIGHT 18
+#define SCREEN_WIDTH 20
+#define WINDOW_HEIGHT 2
+#define WINDOW_WIDTH SCREEN_WIDTH
+#define WINDOW_SIZE WINDOW_WIDTH *WINDOW_HEIGHT
+#define WINDOW_X 7
+#define WINDOW_Y 0
+
+#define camera_max_y ((map_height - SCREEN_HEIGHT) * TILE_SIZE)
+#define camera_max_x ((map_width - SCREEN_WIDTH) * TILE_SIZE)
+
+#define MIN(A, B) ((A) < (B) ? (A) : (B))
+
+uint8_t joy;
+
+// current and old positions of the camera in pixels
+uint16_t camera_x, camera_y, old_camera_x, old_camera_y;
+// current and old position of the map in tiles
+uint8_t map_pos_x, map_pos_y, old_map_pos_x, old_map_pos_y;
+// redraw flag, indicates that camera position was changed
+uint8_t redraw;
+
+// map
+const unsigned char *map;
+const unsigned char *map_attributes;
+short map_width;
+short map_height;
+short map_tile_count;
+
+void set_camera() {
+  // update hardware scroll position
+  SCY_REG = camera_y;
+  SCX_REG = camera_x;
+  // up or down
+  map_pos_y = (uint8_t)(camera_y >> 3u);
+  if (map_pos_y != old_map_pos_y) {
+    if (camera_y < old_camera_y) {
+      set_bkg_submap(map_pos_x, map_pos_y, MIN(21u, map_width - map_pos_x), 1,
+                     map, map_width);
+    } else {
+      if ((map_height - 18u) > map_pos_y)
+        set_bkg_submap(map_pos_x, map_pos_y + 18u,
+                       MIN(21u, map_width - map_pos_x), 1, map, map_width);
+    }
+    old_map_pos_y = map_pos_y;
+  }
+  // left or right
+  map_pos_x = (uint8_t)(camera_x >> 3u);
+  if (map_pos_x != old_map_pos_x) {
+    if (camera_x < old_camera_x) {
+      set_bkg_submap(map_pos_x, map_pos_y, 1, MIN(19u, map_height - map_pos_y),
+                     map, map_width);
+    } else {
+      if ((map_width - 20u) > map_pos_x)
+        set_bkg_submap(map_pos_x + 20u, map_pos_y, 1,
+                       MIN(19u, map_height - map_pos_y), map, map_width);
+    }
+    old_map_pos_x = map_pos_x;
+  }
+  // set old camera position to current camera position
+  old_camera_x = camera_x, old_camera_y = camera_y;
+}
+
+void init_map() {
+  map_pos_x = map_pos_y = 0;
+  old_map_pos_x = old_map_pos_y = 255;
+  set_bkg_submap(map_pos_x, map_pos_y, 20, 18, map, map_width);
+
+  camera_x = camera_y = 0;
+  old_camera_x = camera_x;
+  old_camera_y = camera_y;
+
+  redraw = FALSE;
+
+  SCX_REG = camera_x;
+  SCY_REG = camera_y;
+}
+
+void load_area1() {
+  map = world1area1_map;
+  map_attributes = world1area1_map_attributes;
+  map_width = world1area1_WIDTH;
+  map_height = world1area1_HEIGHT;
+  map_tile_count = world1area1_TILE_COUNT;
+}
+
+void load_area2() {
+  map = world1area2_map;
+  map_attributes = world1area2_map_attributes;
+  map_width = world1area2_WIDTH;
+  map_height = world1area2_HEIGHT;
+  map_tile_count = world1area2_TILE_COUNT;
+}
+
+void interupt() {
+  if (LYC_REG == WINDOW_Y) {
+    SHOW_WIN;
+    LYC_REG = WINDOW_Y + WINDOW_HEIGHT * TILE_SIZE;
+  } else if (LYC_REG == WINDOW_Y + WINDOW_HEIGHT * TILE_SIZE) {
+    HIDE_WIN;
+    LYC_REG = WINDOW_Y;
+  }
+}
+
+// TODO use solid map when available
+bool is_solid(int x, int y) {
+  map[map_width * ((y - OFFSET_Y) / 8) + ((x - OFFSET_X) / 8)];
+}
+
+void main(void) {
+  DISPLAY_ON;
+  SHOW_BKG;
+  SHOW_WIN;
+  SHOW_SPRITES;
+  SPRITES_8x16;
+
+  STAT_REG |= 0x40U;
+  LYC_REG = WINDOW_Y;
+  disable_interrupts();
+  add_LCD(interupt);
+  set_interrupts(LCD_IFLAG | VBL_IFLAG);
+  enable_interrupts();
+
+  // text
+  text_load_font();
+
+  // joypad
+  int joypad_previous, joypad_current = 0;
+
+  // player
+  int player_x = (SCREEN_WIDTH / 2) * TILE_SIZE,
+      player_y = (SCREEN_HEIGHT / 2) * TILE_SIZE;
+  set_sprite_data(0, mario_TILE_COUNT, mario_tiles);
+
+  short level_index = 0;
+  load_area1();
+  init_map();
+
+  set_bkg_data(0, World1Tileset_TILE_COUNT, World1Tileset_tiles);
+
+  short mario_current_frame = 0;
+  short score = 0;
+  short time = 999;
+  short lives = 3;
+  short coins = 0;
+
+  bool mario_flip = FALSE;
+
+  bool debug = FALSE;
+
+  // text
+  char buffer[WINDOW_SIZE];
+  unsigned char windata[WINDOW_SIZE];
+  memset(windata, 15, WINDOW_SIZE);
+  set_win_tiles(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, windata);
+  move_win(WINDOW_X, WINDOW_Y);
+
+  while (1) {
+    // inputs
+    joypad_previous = joypad_current;
+    joypad_current = joypad();
+
+    if (joypad_current & J_SELECT && !(joypad_previous & J_SELECT)) {
+      level_index++;
+      if (level_index == 2) {
+        level_index = 0;
+      }
+
+      if (level_index == 0) {
+        load_area1();
+      } else {
+        load_area2();
+      }
+
+      init_map();
+    }
+
+    if (joypad_current & J_RIGHT && player_x / 8 < map_width * 8) {
+      player_x++;
+      mario_current_frame = (mario_current_frame + 1) % 3;
+      mario_flip = FALSE;
+    }
+    if (joypad_current & J_LEFT && player_x > 12) {
+      player_x--;
+      mario_current_frame = (mario_current_frame + 1) % 3;
+      mario_flip = TRUE;
+    }
+
+    // print text
+    if (debug) {
+      char fmt[] = "X:%d Y:%d\nINDEX:%d TILE:%d";
+      int index = ((player_y - OFFSET_Y) / 8) * map_width + ((player_x - -OFFSET_X) / 8);
+      sprintf(buffer, fmt, (int16_t)player_x - OFFSET_X,
+              (int16_t)player_y - OFFSET_Y, (int16_t)index, map[index]);
+    } else {
+      char fmt[] = "MARIOX%d  WORLD TIME\n %d  Cx%d 1-1  %d";
+      sprintf(buffer, fmt, lives, score, coins, time);
+    }
+    text_print_string_win(0, 0, buffer);
+
+    // gravity
+    if (player_y < SCREEN_HEIGHT * TILE_SIZE)
+      player_y++;
+
+    if (mario_flip)
+      move_metasprite_vflip(mario_metasprites[mario_current_frame], 0, 0, player_x,
+                      player_y);
+    else {
+      move_metasprite(mario_metasprites[mario_current_frame], 0, 0, player_x,
+                      player_y);
+    }
+
+
+    time--;
+
+    wait_vbl_done();
+    if (redraw) {
+      set_camera();
+      redraw = FALSE;
+    }
+  }
+}
