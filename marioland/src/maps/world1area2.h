80204af1e55110dbd63f7ac08ce032d8ba6d0387
odrevet <odrevet@gmail.com>
2023-07-01 23:15:25 +0200

    level layout

--- marioland/src/maps/world1area2.h
+++ marioland/src/maps/world1area2.h
@@ -1,8 +1,8 @@
 #include <gbdk/platform.h>
 
-#define world1area2_TILE_COUNT 720
-#define world1area2_WIDTH 40
+#define world1area2_TILE_COUNT 1440
+#define world1area2_WIDTH 80
 #define world1area2_HEIGHT 18
 
-extern const unsigned char world1area2_map[720];
-extern const unsigned char world1area2_map_attributes[720] ;
+extern const unsigned char world1area2_map[1440];
+extern const unsigned char world1area2_map_attributes[1440] ;
